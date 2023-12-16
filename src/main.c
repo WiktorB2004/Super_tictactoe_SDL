d5a436628c249802308113c8128d68cdee256b68
WiktorB2004 - Ubuntu <byrka.w@gmail.com>
2023-12-16 19:16:33 +0100

    Created todo list @WiktorB2004

--- src/main.c
+++ src/main.c
@@ -2,6 +2,12 @@
 #include "../include/engine.h"
 #include "../include/utils/engine_utils.h"
 
+// TODO: Write game engine code
+// TODO: Write SDL display functions code
+// TODO: Write gameplay code
+// TODO: Write game bot code
+// TODO: Write code for hosting games over network
+
 int main(void)
 {
     printf("Hello World!");
