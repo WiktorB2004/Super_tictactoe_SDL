a4823295fe6da03fdf587faace738b43bcb2440f
WiktorB2004 - Ubuntu <byrka.w@gmail.com>
2024-01-08 20:51:19 +0100

    Add TODO(#11)

--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@
 // TODO(#3): Write gameplay code
 // TODO(#4): Write game bot code
 // TODO(#5): Write code for hosting games over network
+// TODO(#11): Handling edge cases and value limits + improvement of gameplay module
 
 int main(void)
 {
