bbdd609d52c0dd81c44236c34b466b4d2e2a6f1c
Jakub Iliński <kubailinski2@gmail.com>
2024-01-14 22:05:30 +0100

    #4 @Hinski2 dodanie komentarzy do bota 9x9

--- include/game_bot.h
+++ include/game_bot.h
@@ -1,11 +1,11 @@
 /*
-    Implementacja botów o trzech stopniach trudności i w dwruch 
-    wariantach rozmiaru planszy
+    @file bot.c
+    Implementacja botów o trzech stopniach trudności i w dwóch wariantach rozmiaru planszy.
 
-    bot przyjmuje argurmenty:
-        -wskaźnik na plansze do gry
-        -kto wykonuje ruch: X czy O
-        -w której części ma się odbyć ruch, jeśli plansza jest 3x3 to 
+    Bot przyjmuje argumenty:
+        - wskaźnik na planszę do gry
+        - kto wykonuje ruch: X czy O
+        - w której części ma się odbyć ruch, jeśli plansza jest 3x3 to 
             podaj argument = 0, jeśli plansza jest 9x9 to indeksuje części:
             0 | 1 | 2
             ----------
@@ -15,31 +15,99 @@
         - tryb gry:
             ~ 0 plansza 3x3 poziom trudności normal
             ~ 1 plansza 3x3 poziom trudności hard
-            ~ 2 plnasza 3x3 poziom trudności impopable
+            ~ 2 plansza 3x3 poziom trudności impopable
             ~ 3 plansza 9x9 poziom trudności normal
-            ~ 4 plnasza 9x9 poziom trudności hard
-            ~ 5 plnasza 9x9 poziom trudności impobaple
+            ~ 4 plansza 9x9 poziom trudności hard
+            ~ 5 plansza 9x9 poziom trudności impobaple
 
-    co robi bot:
-        -zwraca numer części planszy (patrz linijka 11) w 
-            której trzeba wykonać następny ruch, 0 jeśli grajest 3x3
-        -dodaje do planszy X lub O
-        -jeśli bot nie może wykonać ruchu to zrwóci -1 !!!!!!!
-
+    Co robi bot:
+        - zwraca numer części planszy (patrz linijka 11) w 
+            której trzeba wykonać następny ruch, 0 jeśli gra jest 3x3
+        - dodaje do planszy X lub O
+        - jeśli bot nie może wykonać ruchu to zwróci -1
 */
 
 #pragma once
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * @brief Funkcja realizująca działanie bota w grze kółko-krzyżyk.
+ *
+ * @param plansza Wskaźnik na planszę do gry.
+ * @param gracz Znak określający, kto wykonuje ruch: 'X' lub 'O'.
+ * @param czesc Numer części planszy, w której ma się odbyć ruch.
+ * @param tryb Tryb gry określający poziom trudności i rozmiar planszy.
+ * @return Numer części planszy, w której trzeba wykonać następny ruch, 0 jeśli gra jest 3x3, -1 jeśli bot nie może wykonać ruchu.
+ */
 int bot(char **plansza, char gracz, int czesc, int tryb);
 
+/**
+ * @brief Funkcja realizująca działanie bota w trybie normalnym na planszy 3x3.
+ *
+ * @param plansza Wskaźnik na planszę do gry.
+ * @param gracz Znak określający, kto wykonuje ruch: 'X' lub 'O'.
+ * @param czesc Numer części planszy, w której ma się odbyć ruch.
+ * @return Numer części planszy, w której trzeba wykonać następny ruch, 0 jeśli gra jest 3x3, -1 jeśli bot nie może wykonać ruchu.
+ */
 int bot_3x3_normal(char **plansza, char gracz, int czesc);
+
+/**
+ * @brief Funkcja realizująca działanie bota w trybie trudnym na planszy 3x3.
+ *
+ * @param plansza Wskaźnik na planszę do gry.
+ * @param gracz Znak określający, kto wykonuje ruch: 'X' lub 'O'.
+ * @param czesc Numer części planszy, w której ma się odbyć ruch.
+ * @return Numer części planszy, w której trzeba wykonać następny ruch, 0 jeśli gra jest 3x3, -1 jeśli bot nie może wykonać ruchu.
+ */
 int bot_3x3_hard(char **plansza, char gracz, int czesc);
+
+/**
+ * @brief Funkcja realizująca działanie bota w trybie impopable na planszy 3x3.
+ *
+ * @param plansza Wskaźnik na planszę do gry.
+ * @param gracz Znak określający, kto wykonuje ruch: 'X' lub 'O'.
+ * @param czesc Numer części planszy, w której ma się odbyć ruch.
+ * @return Numer części planszy, w której trzeba wykonać następny ruch, 0 jeśli gra jest 3x3, -1 jeśli bot nie może wykonać ruchu.
+ */
 int bot_3x3_impopable(char **plansza, char gracz, int czesc);
 
+/**
+ * @brief Funkcja realizująca działanie bota w trybie normalnym na planszy 9x9.
+ *
+ * @param plansza Wskaźnik na planszę do gry.
+ * @param gracz Znak określający, kto wykonuje ruch: 'X' lub 'O'.
+ * @param czesc Numer części planszy, w której ma się odbyć ruch.
+ * @return Numer części planszy, w której trzeba wykonać następny ruch, 0 jeśli gra jest 3x3, -1 jeśli bot nie może wykonać ruchu.
+ */
 int bot_9x9_normal(char **plansza, char gracz, int czesc);
+
+/**
+ * @brief Funkcja realizująca działanie bota w trybie trudnym na planszy 9x9.
+ *
+ * @param plansza Wskaźnik na planszę do gry.
+ * @param gracz Znak określający, kto wykonuje ruch: 'X' lub 'O'.
+ * @param czesc Numer części planszy, w której ma się odbyć ruch.
+ * @return Numer części planszy, w której trzeba wykonać następny ruch, 0 jeśli gra jest 3x3, -1 jeśli bot nie może wykonać ruchu.
+ */
 int bot_9x9_hard(char **plansza, char gracz, int czesc);
+
+/**
+ * @brief Funkcja realizująca działanie bota w trybie impopable na planszy 9x9.
+ *
+ * @param plansza Wskaźnik na planszę do gry.
+ * @param gracz Znak określający, kto wykonuje ruch: 'X' lub 'O'.
+ * @param czesc Numer części planszy, w której ma się odbyć ruch.
+ * @return Numer części planszy, w której trzeba wykonać następny ruch, 0 jeśli gra jest 3x3, -1 jeśli bot nie może wykonać ruchu.
+ */
 int bot_9x9_impopable(char **plansza, char gracz, int czesc);
 
+/**
+ * @brief Funkcja realizująca działanie bota w trybie losowym na planszy 9x9.
+ *
+ * @param plansza Wskaźnik na planszę do gry.
+ * @param gracz Znak określający, kto wykonuje ruch: 'X' lub 'O'.
+ * @param czesc Numer części planszy, w której ma się odbyć ruch.
+ * @return Numer części planszy, w której trzeba wykonać następny ruch, 0 jeśli gra jest 3x3, -1 jeśli bot nie może wykonać ruchu.
+ */
 int bot_9x9_random(char **plansza, char gracz, int czesc);
--- include/utils/bot_utils.h
+++ include/utils/bot_utils.h
@@ -5,88 +5,124 @@
 #include <time.h>
 #include <pthread.h>
 
+/**
+ * @brief Struktura przechowująca informacje o ruchu w grze.
+ */
 typedef struct zmiana
 {
-    int x;
-    int y;
-    int czesc;
-    char gracz;
+    int x;       /**< Współrzędna X ruchu */
+    int y;       /**< Współrzędna Y ruchu */
+    int czesc;   /**< Numer części planszy w którym ma się odbyć następny ruch */
+    char gracz;  /**< Symbol gracza (X lub O) */
 } zmiana;
 
+/**
+ * @brief Struktura przechowująca współrzędne na planszy.
+ */
 typedef struct pair
 {
-    int x;
-    int y;
+    int x;  /**< Współrzędna X */
+    int y;  /**< Współrzędna Y */
 } pair;
 
+/**
+ * @brief Struktura przechowująca dynamiczną tablicę wskaźników na synów węzła.
+ */
 typedef struct vector_node
 {
-    int size;
-    int max_size;
-    struct node **sons; // wskaźnik na tablice wskaźników, które wskazują na synów typu node
+    int size;                /**< Aktualny rozmiar tablicy */
+    int max_size;            /**< Maksymalny rozmiar tablicy */
+    struct node **sons;      /**< Wskaźnik na tablicę wskaźników na synów typu node */
 } vector_node;
 
+/**
+ * @brief Struktura reprezentująca węzeł drzewa poszukiwań Monte Carlo.
+ */
 typedef struct node
 {
-    struct node *parent;
-    vector_node *vec;
-
-    zmiana ruch;
-    int visit;
-    int wins;
-    pthread_mutex_t *mutex;
+    struct node *parent;         /**< Wskaźnik na rodzica węzła */
+    vector_node *vec;            /**< Wskaźnik na dynamiczną tablicę wskaźników na synów typu node */
+    zmiana ruch;                 /**< Informacje o ruchu */
+    int visit;                   /**< Liczba odwiedzin węzła */
+    int wins;                    /**< Liczba zwycięstw w symulacjach związanych z węzłem */
+    pthread_mutex_t *mutex;      /**< Wskaźnik na muteks dla bezpiecznej synchronizacji węzłów w wielowątkowym środowisku */
 } node;
 
-typedef struct argument{
-    char **plansza;
-    char **nad_zwyciestwa;
-    char gracz;
-    int czesc;
-    node *v;
-
+/**
+ * @brief Struktura przechowująca dane dla funkcji głównej.
+ */
+typedef struct argument
+{
+    char **plansza;   /**< Wskaźnik na planszę gry */
+    char **nad_zwyciestwa;  /**< Wskaźnik na pomocniczą planszę do sprawdzania zwycięstwa */
+    char gracz;       /**< Symbol aktualnego gracza */
+    int czesc;        /**< Numer części planszy w którym ma się odbyć ruch*/
+    node *v;          /**< Wskaźnik na korzeń drzewa poszukiwań Monte Carlo */
 } argument;
 
-typedef struct choosen_node{
-    node *v;
-    int idx;
+/**
+ * @brief Struktura przechowująca informacje o wybranym węźle.
+ */
+typedef struct choosen_node
+{
+    node *v;  /**< Wskaźnik na wybrany węzeł */
+    int idx;   /**< Indeks wybranego węzła */
 } choosen_node;
 
-// dane
+/** @name Funkcje inicjalizujące i usuwające struktury danych */
+///@{
 node *create_node(char **plansza, int czesc);
 vector_node *create_vector_node(char **plansza, int czesc);
 void destruct_node(node *NODE);
 void destruct_vector_node(vector_node *vector);
 void push_back(choosen_node *cn, node *new_node);
+///@}
 
-// funkcje do sprawdzania wyniku potyczek na planszy
+/** @name Funkcje sprawdzające wyniki potyczek na planszy */
+///@{
 char sprawdz_wynik(char **sub_plansza);
 void uzupelnij_nad_zwyciestwa(char **plansza, char **nad_zwyciestwa);
 void update_nad_zwyciestwa(char **plnasza, char **nad_zwyciestwa, int czesc);
+///@}
 
-// funkcje do selection
+/** @name Funkcje do selection */
+///@{
 double uct(node *wierzcholek);
 int pelny(char **plansza, int czesc, node *v);
 choosen_node *Select(char **plansza, node *v, char **nad_zwyciestwa);
 int remis(char **nad_zwyciestwa);
+///@}
 
-// funkcje do expansion
+/** @name Funkcje do expansion */
+///@{
 int dodaj_syna(choosen_node *v, char **plansza, char **nad_zwyciestwa);
+///@}
 
-// funkcje do simulation
+/** @name Funkcje do simulation */
+///@{
 int symulate(node *v, char **plansza, char **nad_zwyciestwa, char gracz);
+///@}
 
-// funkcje do backpropagation
+/** @name Funkcje do backpropagation */
+///@{
 void unselect(node *v, char **plansza, int wynik, char **nad_zwyciestwa);
+///@}
 
-// pomieszane funkcje
+/** @name Funkcje pomocnicze */
+///@{
 char zmiana_gracza(char gracz);
 int znajdz_czesc(zmiana ruch);
 pair poczatek_czesci(int czesc);
 zmiana znajdz_opt(node *v);
+///@}
 
-//wypisywanie
+/** @name Funkcje wypisywania (do debugowania)*/
+///@{
 void cout(char **plansza, int x);
+///@}
 
-//alokacja i dealokacja 
+/** @name Funkcje alokacji i dealokacji pamięci */
+///@{
 char **allocate(int x);
 void deallocate(char **matrix, int x);
+///@}
