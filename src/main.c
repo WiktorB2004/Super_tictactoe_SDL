a3470e9b30d61c83f80a1e16326c47dcc89e6ebd
WiktorB2004 - Ubuntu <byrka.w@gmail.com>
2024-01-21 12:32:47 +0100

    Remove TODO(#13)

--- src/main.c
+++ src/main.c
@@ -7,7 +7,6 @@
 // TODO(#4): Write game bot code
 // TODO(#5): Write code for hosting games over network
 // TODO(#11): Handling edge cases and value limits + improvement of gameplay module
-// TODO(#13): Containerize the project and create workflow to deploy image to GH Packages / Docker Hub
 // TODO(#16): Improve GUI - SDL functions
 
 int main(void)
