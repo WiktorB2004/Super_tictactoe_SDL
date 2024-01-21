fa56f794590549fd13fbb8fd699c745e99a0bf98
Jakub Iliński <kubailinski2@gmail.com>
2024-01-21 02:15:01 +0100

    naprawa

--- test/bot_test/include/bot_test.h
+++ test/bot_test/include/bot_test.h
@@ -8,4 +8,27 @@
 #include "../../../include/utils/gameplay_utils.h"
 
 extern Game *game;
+
+void test_bot0(void);
+void test_bot1(void);
+void test_bot2(void);
+void test_bot3(void);
+void test_bot4(void);
+void test_bot5(void);
+void test_bot6(void);
+void test_bot7(void);
+void test_bot8(void);
+void test_bot9(void);
+void test_bot10(void);
+void test_bot11(void);
+void test_bot12(void);
+void test_bot13(void);
+void test_bot14(void);
+void test_bot15(void);
+void test_bot16(void);
+void test_bot17(void);
+void test_bot18(void);
+void test_bot19(void);
+
+
 #endif // BOT_TEST_H
