e6162f086befb75dd58306b5f55095c4ff119eee
WiktorB2004 - Ubuntu <byrka.w@gmail.com>
2024-01-24 21:18:28 +0100

    Updated TODO list @WiktorB2004

--- src/main.c
+++ src/main.c
@@ -2,12 +2,11 @@
 #include "../include/engine.h"
 #include "../include/utils/engine_utils.h"
 
-// TODO(#1): Write game engine code
-// TODO(#2): Write SDL display functions code
-// TODO(#4): Write game bot code
 // TODO(#5): Write code for hosting games over network
 // TODO(#11): Handling edge cases and value limits + improvement of gameplay module
 // TODO(#16): Improve GUI - SDL functions
+// TODO: Visual improvements
+// TODO: Improve documentation
 
 int main(void)
 {
